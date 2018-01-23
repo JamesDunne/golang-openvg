2c6770bf4fe173b65659e085e21529cb288016d2
James Dunne <james.jdunne@gmail.com>
2018-01-22 23:39:00 -0600

    Added precursor to darwin port using AmanithVG library.

---
+++ vg_darwin.go
@@ -0,0 +1,86 @@
+//+build darwin
+
+package vg
+
+/*
+#cgo CFLAGS: -x objective-c -Iinclude
+#cgo LDFLAGS: -framework Cocoa -framework OpenGL -framework QuartzCore -Llib/macosx/ub/gle/standalone -lAmanithVG
+
+#include "macosx/main.m"
+
+void
+appInit(const VGint surfaceWidth, const VGint surfaceHeight)
+{
+
+}
+
+void
+appDestroy(void)
+{
+
+}
+
+void
+appResize(const VGint surfaceWidth, const VGint surfaceHeight)
+{
+
+}
+
+void
+appDraw(const VGint surfaceWidth, const VGint surfaceHeight)
+{
+
+}
+
+// handle mouse events
+void
+mouseLeftButtonDown(const VGint x, const VGint y)
+{
+
+}
+
+void
+mouseLeftButtonUp(const VGint x, const VGint y)
+{
+
+}
+
+void
+mouseRightButtonDown(const VGint x, const VGint y)
+{
+}
+
+void
+mouseRightButtonUp(const VGint x, const VGint y)
+{
+}
+
+
+void
+mouseMove(const VGint x, const VGint y)
+{
+}
+
+
+// handle touch events
+void
+touchDoubleTap(const VGint x, const VGint y)
+{
+}
+
+void
+touchPinch(const VGfloat deltaScl)
+{
+}
+
+void
+touchRotate(const VGfloat deltaRot)
+{
+}
+
+*/
+import "C"
+
+func main() {
+	C.vgMain(800, 480)
+}
