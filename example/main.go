0d9a56e9f9ef7982640fc91e5436cea471a74dc9
James Dunne <james.jdunne@gmail.com>
2018-01-28 10:45:19 -0600

    Dumb example of using vg bindings. Wont do anything yet.

--- example/main.go
+++ example/main.go
@@ -4,10 +4,13 @@ package main
 import (
 	"fmt"
 
-	vg "github.com/JamesDunne/golang-openvg"
+	vgf "github.com/JamesDunne/golang-openvg"
+	vg "github.com/JamesDunne/golang-openvg/vg"
 )
 
 func main() {
 	fmt.Println("Hello World!")
-	vg.Run(800, 480)
+	vgf.Run(800, 480)
+	vg.Setfv(vg.ClearColor, 4, &[]float32{1.0, 0.0, 0.0, 1.0}[0])
+	vg.Clear(0, 0, 800, 480)
 }
