cbc6a8c42034068447d143d481d61b81486420f1
James Dunne <james.jdunne@gmail.com>
2018-01-29 14:21:28 -0600

    Using vgu.RoundRect as example.

--- example/main.go
+++ example/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/JamesDunne/golang-openvg/host"
 	"github.com/JamesDunne/golang-openvg/vg"
+	"github.com/JamesDunne/golang-openvg/vgu"
 )
 
 func initVG(width, height int32) {
@@ -27,8 +28,16 @@ func initVG(width, height int32) {
 func drawVG(width, height int32) {
 	vg.Clear(0, 0, width, height)
 
-	//color := vg.CreatePaint()
-	//vg.SetPaint(color, uint32(vg.FillPath|vg.StrokePath))
+	color := vg.CreatePaint()
+	vg.SetParameteri(color, int32(vg.PaintType), int32(vg.PaintTypeColor))
+	vg.SetParameterfv(color, int32(vg.PaintColor), 4, &[]float32{0.0, 1.0, 0.0, 1.0}[0])
+
+	vg.SetPaint(color, uint32(vg.FillPath|vg.StrokePath))
+
+	path := vg.CreatePath(0 /* vg.PathFormatStandard */, vg.PathDatatypeF, 1.0, 0.0, 0, 0, uint32(vg.PathCapabilityAll))
+	vgu.RoundRect(path, 3, 3, 400, 100, 9, 9)
+	vg.DrawPath(path, uint32(vg.FillPath))
+	vg.DestroyPath(path)
 }
 
 func main() {
