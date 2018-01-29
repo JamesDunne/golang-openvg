be501cfc0a2e0fc94cf8d2bf3c19d8a5b2cc9d09
James Dunne <james.jdunne@gmail.com>
2018-01-29 11:45:18 -0600

    Use init and draw functions.

--- example/main.go
+++ example/main.go
@@ -8,19 +8,38 @@ import (
 	"github.com/JamesDunne/golang-openvg/vg"
 )
 
+func initVG(width, height int32) {
+	clearColor := &[]float32{0.0, 0.0, 1.0, 1.0}[0]
+	tileColor := &[]float32{0.0, 1.0, 0.0, 1.0}[0]
+
+	// set some default parameters for the OpenVG context
+	vg.Seti(vg.FillRule, int32(vg.EvenOdd))
+	vg.Setfv(vg.ClearColor, 4, clearColor)
+	vg.Setfv(vg.TileFillColor, 4, tileColor)
+	vg.Setf(vg.StrokeLineWidth, 20.0)
+	vg.Seti(vg.StrokeCapStyle, int32(vg.CapButt))
+	vg.Seti(vg.StrokeJoinStyle, int32(vg.JoinBevel))
+	vg.Seti(vg.RenderingQuality, int32(vg.RenderingQualityBetter))
+	vg.Seti(vg.BlendMode, int32(vg.BlendSrc))
+}
+
 // Rendering is done on a specific OS thread managed by host:
-func draw(width, height int32) {
-	vg.Setfv(vg.ClearColor, 4, &[]float32{0.0, 0.0, 1.0, 1.0}[0])
+func drawVG(width, height int32) {
 	vg.Clear(0, 0, width, height)
+
+	//color := vg.CreatePaint()
+	//vg.SetPaint(color, uint32(vg.FillPath|vg.StrokePath))
 }
 
 func main() {
 	fmt.Println("Hello World!")
 
+	// Supply Go function callbacks:
+	host.InitFunc = initVG
+	host.DrawFunc = drawVG
+
 	// Initialize the host to display OpenVG graphics at 800x480 resolution:
 	host.Init(800, 480)
-	// Supply Go function callback for rendering:
-	host.DrawFunc = draw
 
 	// Event polling with idle loop:
 	for host.PollEvent() {
