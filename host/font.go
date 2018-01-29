5013fdab73943ee51599217dbcee1e2ae5accb25
James Dunne <james.jdunne@gmail.com>
2018-01-29 16:11:53 -0600

    Add hard-coded sans font.

---
+++ host/font.go
@@ -0,0 +1,102 @@
+// font
+package host
+
+//#include "DejaVuSans.h"
+import "C"
+
+import (
+	"unsafe"
+
+	"github.com/JamesDunne/golang-openvg/vg"
+)
+
+type Font struct {
+	count           int
+	descenderHeight C.int
+	fontHeight      C.int
+	characterMap    []C.short
+	glyphAdvances   []C.int
+	glyphs          []uint32
+}
+
+func NewSansFont() *Font {
+	count := C.DejaVuSans_glyphCount
+
+	f := &Font{
+		count:           count,
+		fontHeight:      C.DejaVuSans_font_height,
+		descenderHeight: C.DejaVuSans_descender_height,
+		characterMap:    make([]C.short, 500),
+		glyphAdvances:   make([]C.int, count),
+		glyphs:          make([]uint32, count),
+	}
+
+	copy(f.characterMap, C.DejaVuSans_characterMap[:])
+	copy(f.glyphAdvances, C.DejaVuSans_glyphAdvances[:])
+
+	for i := 0; i < count; i++ {
+		p := (*int32)(&C.DejaVuSans_glyphPoints[C.DejaVuSans_glyphPointIndices[i]*2])
+		instructions := (*uint8)(&C.DejaVuSans_glyphInstructions[C.DejaVuSans_glyphInstructionIndices[i]])
+		ic := int32(C.DejaVuSans_glyphInstructionCounts[i])
+		path := vg.CreatePath(
+			0, /* VG_PATH_FORMAT_STANDARD */
+			vg.PathDatatypeS32,
+			1.0/65536.0, 0.0, 0, 0,
+			uint32(vg.PathCapabilityAll))
+		f.glyphs[i] = path
+		if ic > 0 {
+			vg.AppendPathData(path, ic, instructions, (*byte)(unsafe.Pointer(p)))
+		}
+	}
+
+	return f
+}
+
+func Text(s string, f *Font) {
+	//	float32 size = (VGfloat) pointsize, xx = x, mm[9];
+	size := float32(20.0)
+	x := float32(0)
+	y := float32(0)
+	xx := x
+	var mm [9]float32
+
+	//	vg.GetMatrix(mm);
+	vg.GetMatrix(&mm[0])
+
+	//	int character;
+	//	unsigned char *ss = (unsigned char *)s;
+	//	while ((ss = next_utf8_char(ss, &character)) != NULL) {
+	for _, character := range s {
+		//		int glyph = f.CharacterMap[character];
+		//		if (glyph == -1) {
+		//			continue;			   //glyph is undefined
+		//		}
+		glyph := f.characterMap[character]
+		if glyph == -1 {
+			continue
+		}
+
+		//		VGfloat mat[9] = {
+		//			size, 0.0f, 0.0f,
+		//			0.0f, size, 0.0f,
+		//			xx, y, 1.0f
+		//		};
+		mat := [9]float32{
+			size, 0, 0,
+			0, size, 0,
+			xx, y, 1,
+		}
+		//		vgLoadMatrix(mm);
+		//		vgMultMatrix(mat);
+		vg.LoadMatrix(&mm[0])
+		vg.MultMatrix(&mat[0])
+		//		vgDrawPath(f.Glyphs[glyph], VG_FILL_PATH);
+		vg.DrawPath(f.glyphs[glyph], uint32(vg.FillPath))
+		//		xx += size * f.GlyphAdvances[glyph] / 65536.0f;
+		xx += size * float32(f.glyphAdvances[glyph]) / 65536.0
+		//	}
+	}
+
+	//	vgLoadMatrix(mm);
+	vg.LoadMatrix(&mm[0])
+}
