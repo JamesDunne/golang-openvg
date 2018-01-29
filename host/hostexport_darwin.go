463d26a1bb0f14e423d0083976908b5d1495d6cd
James Dunne <james.jdunne@gmail.com>
2018-01-29 12:42:12 -0600

    Rename vg* to host* and move callback types and func variables to common file.

--- host/hostexport_darwin.go
+++ host/hostexport_darwin.go
@@ -90,20 +90,3 @@ func keyUp(k uint16) {
 		cb(k)
 	}
 }
-
-type SurfaceCallbackFunc func(width, height int32)
-type VoidCallbackFunc func()
-type PositionCallbackFunc func(x, y int32)
-type KeyCallbackFunc func(k uint16)
-
-var InitFunc SurfaceCallbackFunc = nil
-var DestroyFunc VoidCallbackFunc = nil
-var ResizeFunc SurfaceCallbackFunc = nil
-var DrawFunc SurfaceCallbackFunc = nil
-var MouseLeftButtonDownFunc PositionCallbackFunc = nil
-var MouseLeftButtonUpFunc PositionCallbackFunc = nil
-var MouseRightButtonDownFunc PositionCallbackFunc = nil
-var MouseRightButtonUpFunc PositionCallbackFunc = nil
-var MouseMoveFunc PositionCallbackFunc = nil
-var KeyDownFunc KeyCallbackFunc = nil
-var KeyUpFunc KeyCallbackFunc = nil
