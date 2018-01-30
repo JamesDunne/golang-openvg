beee9f0dc634b5f2986e873054baf67bd8e19954
James Dunne <james.jdunne@gmail.com>
2018-01-30 12:55:41 -0600

    Example removed vg dependency using vgui.

--- example/main.go
+++ example/main.go
@@ -5,50 +5,30 @@ import (
 	"runtime"
 
 	"github.com/JamesDunne/golang-openvg/host"
-	"github.com/JamesDunne/golang-openvg/vg"
-	"github.com/JamesDunne/golang-openvg/vgu"
 	"github.com/JamesDunne/golang-openvg/vgui"
 )
 
 var f *vgui.Font
+var ui *vgui.UI
 
 func initVG(width, height int32) {
-	f = vgui.NewSansFont()
+	ui = vgui.NewUI()
 
-	clearColor := &[]float32{0.0, 0.0, 1.0, 1.0}[0]
-	tileColor := &[]float32{0.0, 1.0, 0.0, 1.0}[0]
-
-	// set some default parameters for the OpenVG context
-	//vg.Seti(vg.FillRule, int32(vg.EvenOdd))
-	vg.Seti(vg.FillRule, int32(vg.NonZero))
-
-	vg.Setfv(vg.ClearColor, 4, clearColor)
-	vg.Setfv(vg.TileFillColor, 4, tileColor)
-	vg.Setf(vg.StrokeLineWidth, 0.125)
-	vg.Seti(vg.StrokeCapStyle, int32(vg.CapButt))
-	vg.Seti(vg.StrokeJoinStyle, int32(vg.JoinBevel))
-	vg.Seti(vg.RenderingQuality, int32(vg.RenderingQualityBetter))
-	vg.Seti(vg.BlendMode, int32(vg.BlendSrc))
+	ui.Init()
+	ui.SetWindow(vgui.NewWindow(0, 0, float32(width), float32(height)))
 }
 
 // Rendering is done on a specific OS thread managed by host:
 func drawVG(width, height int32) {
-	vg.LoadIdentity()
-	vg.Clear(0, 0, width, height)
+	// Update display window:
+	ui.SetWindow(vgui.NewWindow(0, 0, float32(width), float32(height)))
 
-	color := vg.CreatePaint()
-	vg.SetParameteri(color, int32(vg.PaintType), int32(vg.PaintTypeColor))
-	vg.SetParameterfv(color, int32(vg.PaintColor), 4, &[]float32{1.0, 1.0, 1.0, 0.8}[0])
+	ui.BeginFrame()
 
-	vg.SetPaint(color, uint32(vg.FillPath|vg.StrokePath))
+	ui.FillColor(ui.Palette(4))
+	ui.TextPoint(vgui.Point{150, 200}, 20, vgui.AlignLeft, "Tripping on a Hole in a Paper Heart")
 
-	_ = vgu.ArcChord
-	//	path := vg.CreatePath(0 /* vg.PathFormatStandard */, vg.PathDatatypeF, 1.0, 0.0, 0, 0, uint32(vg.PathCapabilityAll))
-	//	vgu.RoundRect(path, 3, 3, 400, 100, 9, 9)
-	//	vg.DrawPath(path, uint32(vg.FillPath))
-	//	vg.DestroyPath(path)
-
-	vgui.Text("Tripping on a Hole in a Paper Heart", 150, 200, 20, f)
+	ui.EndFrame()
 }
 
 func main() {
