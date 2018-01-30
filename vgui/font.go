3e7592b568d5efcb07971432a344014c97e86bf9
James Dunne <james.jdunne@gmail.com>
2018-01-30 14:54:39 -0600

    Split Text into functions to avoid allocation during rendering.

--- vgui/font.go
+++ vgui/font.go
@@ -79,34 +79,52 @@ func NewSansFont() *Font {
 	return f
 }
 
-func Text(s string, x, y, size float32, align Alignment, f *Font) {
-	mm := vg.Geti(vg.MatrixMode)
+type RenderedText struct {
+	String string
 
-	if mm != int32(vg.MatrixGlyphUserToSurface) {
-		vg.Seti(vg.MatrixMode, int32(vg.MatrixGlyphUserToSurface))
+	font    *Font
+	runes   []uint32
+	adjustX []float32
+	adjustY []float32
+	width   float32
+}
+
+func NewText(s string, f *Font) *RenderedText {
+	t := &RenderedText{
+		String: s,
+		font:   f,
 	}
-	vg.Seti(vg.FillRule, int32(vg.NonZero))
 
-	runes := make([]uint32, utf8.RuneCountInString(s))
-	adjustX := make([]float32, len(runes))
-	adjustY := make([]float32, len(runes))
-	width := float32(0)
+	t.runes = make([]uint32, utf8.RuneCountInString(s))
+	t.adjustX = make([]float32, len(t.runes))
+	t.adjustY = make([]float32, len(t.runes))
+
+	t.width = float32(0)
 	for i, character := range s {
 		glyph := f.characterMap[character]
 		if glyph == -1 {
 			continue
 		}
-		runes[i] = uint32(glyph)
+
+		t.runes[i] = uint32(glyph)
+
 		// TODO: kerning
-		adjustX[i] = 0
-		adjustY[i] = 0
-		width += f.glyphAdvances[glyph]
+		t.adjustX[i] = 0
+		t.adjustY[i] = 0
+
+		t.width += f.glyphAdvances[glyph]
 	}
 
+	return t
+}
+
+func TextRendered(t *RenderedText, x, y, size float32, align Alignment) {
+	f := t.font
+
 	if align&AlignCenter == AlignCenter {
-		x -= width * size * 0.5
+		x -= t.width * size * 0.5
 	} else if align&AlignRight == AlignRight {
-		x -= width * size
+		x -= t.width * size
 	}
 	y += f.descenderHeight * size
 	if align&AlignMiddle == AlignMiddle {
@@ -115,13 +133,24 @@ func Text(s string, x, y, size float32, align Alignment, f *Font) {
 		y += (f.fontHeight - f.descenderHeight) * size
 	}
 
+	mm := vg.Geti(vg.MatrixMode)
+	if mm != int32(vg.MatrixGlyphUserToSurface) {
+		vg.Seti(vg.MatrixMode, int32(vg.MatrixGlyphUserToSurface))
+	}
+
+	vg.Seti(vg.FillRule, int32(vg.NonZero))
 	vg.Setfv(vg.GlyphOrigin, 2, &[]float32{0, 0}[0])
 	vg.LoadIdentity()
 	vg.Translate(x, y)
 	vg.Scale(size, size)
-	vg.DrawGlyphs(f.vgHandle, int32(len(runes)), &runes[0], &adjustX[0], &adjustY[0], uint32(vg.FillPath), vg.False)
+	vg.DrawGlyphs(f.vgHandle, int32(len(t.runes)), &t.runes[0], &t.adjustX[0], &t.adjustY[0], uint32(vg.FillPath), vg.False)
 
 	if mm != int32(vg.MatrixGlyphUserToSurface) {
 		vg.Seti(vg.MatrixMode, mm)
 	}
+}
+
+func Text(s string, x, y, size float32, align Alignment, f *Font) {
+	t := NewText(s, f)
+	TextRendered(t, x, y, size, align)
 }
